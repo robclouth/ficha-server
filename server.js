d19a70c9f561facca412aa01fedf96f55ecff694
Rob Clouth <rob.clouth@gmail.com>
2020-04-25 18:47:45 +0200

    Minor change

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { createServer } = require("cors-anywhere");
 const checkRateLimit = require("cors-anywhere/lib/rate-limit");
 
 PeerServer({ port: 9000, path: "/peerjs" });
+
 createServer({
   checkRateLimit: checkRateLimit(
     "0 1 ficha-git-develop.robclouth.now.sh ficha.now.sh"
